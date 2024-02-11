6cfca0e10c5ee0421dc6d4675ee8b2442bbbc678
DylanuCom <dylanucom@gmail.com>
2024-02-11 23:28:46 +0200

    update

--- src/CurdmixServiceProvider.php
+++ src/CurdmixServiceProvider.php
@@ -13,10 +13,11 @@ class CurdmixServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // قم بنقل الملف MakeAllCommand.php إلى المسار المطلوب
-        $this->publishes([
-            __DIR__.'/Console/Commands/MakeAllCommand.php' => app_path('Console/Commands/MakeAllCommand.php'),
-        ], 'curdmix-command');
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/Console/Commands/MakeAllCommand.php' => app_path('Console/Commands/MakeAllCommand.php'),
+            ], 'curdmix-command');
+        }
     }
 
     /**
